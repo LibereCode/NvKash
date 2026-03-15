2d08429fb3f9f04bc8c80ece5ff0ae9d51b78e83
Libere Code <libere-code@posteo.org>
2026-03-15 01:32:03 +0100

    plugins
    added suda.nvim to oneliners. Updated telescope and whichkey with better keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -77,17 +77,24 @@ return { -- Fuzzy Finder (files, lsp, etc)
     pcall(require('telescope').load_extension, 'ui-select')
 
     -- See `:help telescope.builtin`
+
+    -- search/select
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
+    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = 'select Telescope' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = 'word' })
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'grep' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'diagnostics' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'resume' })
+
+    -- find/file
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'help' })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'keymaps' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'files' })
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'recent Files' })
+    vim.keymap.set('n', '<leader>f:', builtin.commands, { desc = '[:]commands' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'buffers' })
+    vim.keymap.set('n', '<leader>fc', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = 'nvim [c]onfig' }) -- Shortcut for searching your Neovim configuration files
+
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
@@ -98,29 +105,29 @@ return { -- Fuzzy Finder (files, lsp, etc)
         local buf = event.buf
 
         -- Find references for the word under your cursor.
-        vim.keymap.set('n', 'grr', builtin.lsp_references, { buffer = buf, desc = '[G]oto [R]eferences' })
+        vim.keymap.set('n', 'grr', builtin.lsp_references, { buffer = buf, desc = 'references' })
 
         -- Jump to the implementation of the word under your cursor.
         -- Useful when your language has ways of declaring types without an actual implementation.
-        vim.keymap.set('n', 'gri', builtin.lsp_implementations, { buffer = buf, desc = '[G]oto [I]mplementation' })
+        vim.keymap.set('n', 'gri', builtin.lsp_implementations, { buffer = buf, desc = 'implementation' })
 
         -- Jump to the definition of the word under your cursor.
         -- This is where a variable was first declared, or where a function is defined, etc.
         -- To jump back, press <C-t>.
-        vim.keymap.set('n', 'grd', builtin.lsp_definitions, { buffer = buf, desc = '[G]oto [D]efinition' })
+        vim.keymap.set('n', 'grd', builtin.lsp_definitions, { buffer = buf, desc = 'definition' })
 
         -- Fuzzy find all the symbols in your current document.
         -- Symbols are things like variables, functions, types, etc.
-        vim.keymap.set('n', 'gO', builtin.lsp_document_symbols, { buffer = buf, desc = 'Open Document Symbols' })
+        vim.keymap.set('n', 'gO', builtin.lsp_document_symbols, { buffer = buf, desc = '[O]pen document Symbols' })
 
         -- Fuzzy find all the symbols in your current workspace.
         -- Similar to document symbols, except searches over your entire project.
-        vim.keymap.set('n', 'gW', builtin.lsp_dynamic_workspace_symbols, { buffer = buf, desc = 'Open Workspace Symbols' })
+        vim.keymap.set('n', 'gW', builtin.lsp_dynamic_workspace_symbols, { buffer = buf, desc = 'open [W]orkspace Symbols' })
 
         -- Jump to the type of the word under your cursor.
         -- Useful when you're not sure what type a variable is and you want to see
         -- the definition of its *type*, not where it was *defined*.
-        vim.keymap.set('n', 'grt', builtin.lsp_type_definitions, { buffer = buf, desc = '[G]oto [T]ype Definition' })
+        vim.keymap.set('n', 'grt', builtin.lsp_type_definitions, { buffer = buf, desc = 'type definition' })
       end,
     })
 
@@ -131,7 +138,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
         winblend = 10,
         previewer = false,
       })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    end, { desc = 'Fzf [/] current buf' })
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
@@ -144,10 +151,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
           prompt_title = 'Live Grep in Open Files',
         }
       end,
-      { desc = '[S]earch [/] in Open Files' }
+      { desc = 'Fzf [/] current files' }
     )
-
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
   end,
 }
