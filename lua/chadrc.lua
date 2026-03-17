8de06c1cead55a910f84c2c4e7c927d6d84774dd
Libere Code <libere-code@posteo.org>
2026-03-17 23:19:32 +0100

    charc config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,58 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "chadracula-evondev",
+  theme = "doomchad",
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+  transparency = true,
+}
+
+M.ui = {
+  tabufline = {
+    lazyload = false,
+    order = { "treeOffset", "buffers", "tabs", "never_goon", "btns" },
+    modules = {
+      never_goon = function()
+        return "never😂goon "
+      end,
+    },
+  },
+
+  telescope = { style = "bordered" },
+  statusline = {
+    theme = "default",
+    separator_style = "default",
+    -- order = { "mode", "f", "git", "%=", "lsp_msg", "%=", "lsp", "cwd", "suck" },
+    -- modules = {
+    --   f = "%F", -- full path
+    --   suck = "%L %l", -- Total line, current line
+    -- },
+  },
+  -- tabufline = {},
+  cmp = { -- what is this?
+    style = "atom_colored",
+  },
 }
 
 M.nvdash = { load_on_startup = true }
-M.ui = {
-      tabufline = {
-         lazyload = false
-     }
+
+-- M.term = {}
+
+-- M.lsp = {}
+-- M.mason = {}
+
+-- M.cheatsheet = {}
+
+M.colorify = {
+  virt_text = " ", -- #ff7200
 }
 
 return M
