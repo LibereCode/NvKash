fbc55c07e032404916070652549aba9497e4f6d2
Libere Code <libere-code@posteo.org>
2026-03-19 22:27:31 +0100

    forever pain

--- lua/custom/lazygit.lua
+++ lua/custom/lazygit.lua
@@ -1,35 +1,27 @@
 -- LazyGit💤
 
--- version 3.4 -- HACK: ClankerGPT is fucking retarded
+-- version 1.23 -- ClankerGPT is fucking retarded
 local M = {}
-local lg_buf = nil
-local lg_win = nil
+local buf_lg = nil
+local win_lg = nil
 
 function M.toggle()
    -- Close window and buffer if window exists
-   if lg_win and vim.api.nvim_win_is_valid(lg_win) then -- is really both statements needed?
-      vim.api.nvim_win_close(lg_win, true)
-      if lg_buf and vim.api.nvim_buf_is_valid(lg_buf) then vim.api.nvim_buf_delete(lg_buf, { force = true }) end
-      lg_win = nil
-      lg_buf = nil
-      return -- XXX: enable toggle?
+   if win_lg and vim.api.nvim_win_is_valid(win_lg) then -- is really both statements needed?
+      vim.api.nvim_win_close(win_lg, true)
+      if buf_lg and vim.api.nvim_buf_is_valid(buf_lg) then vim.api.nvim_buf_delete(buf_lg, { force = true }) end
+      win_lg = nil
+      buf_lg = nil
+      return -- XXX: enables toggle?
    end
 
-   if not lg_buf or not vim.api.nvim_buf_is_valid(lg_buf) then -- if no lg_buf (always true) then
-      lg_buf = vim.api.nvim_create_buf(false, true) -- NOTE: Create fresh (unlisted, scratch) buffer and return bufnr
+   if not buf_lg or not vim.api.nvim_buf_is_valid(buf_lg) then -- if no lg_buf (always true) then
+      buf_lg = vim.api.nvim_create_buf(false, true) -- NOTE: Create fresh (unlisted, scratch) buffer and return bufnr
    end
 
-   lg_win = vim.api.nvim_open_win(lg_buf, true, { -- Open floating window
-      relative = 'editor',
-      width = math.floor(vim.o.columns * 0.9),
-      height = math.floor(vim.o.lines * 0.9),
-      row = math.floor(vim.o.lines * 0.05),
-      col = math.floor(vim.o.columns * 0.05),
-      border = 'rounded',
-   })
+   win_lg = vim.api.nvim_open_win(buf_lg, true, require 'custom.dimension'(0.9)) -- Open floating window
 
-   -- Run lazygit directly in the terminal buffer
-   vim.cmd 'terminal lazygit'
+   vim.cmd 'terminal lazygit' -- Run lazygit in terminal buf
    vim.cmd 'startinsert'
 end
 
