4116a4f6d32345bdbc430bd0fac5717316dc43af
Libere Code <libere-code@posteo.org>
2026-03-23 23:10:21 +0100

    soft tweaks-improvements

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,194 +4,271 @@
 -- > Alpha (dashboard)
 -- > Lualine (GOAT)
 -- > scope (tab-manager)
+-- > bufferline (buffer-bar)
 -- DISBLED:
 -- > barbar (buffer/tab-bar)
 -- > cokeline (buffer-bar)
--- > bufferline (buffer-bar)
 
 return {
-   { -- NOTE: Colorizer (give color to #ff7200 hex codes) https://github.com/norcalli/nvim-colorizer.lua
-      'norcalli/nvim-colorizer.lua',
-      config = function() require('colorizer').setup() end,
-   },
+  { -- NOTE: Colorizer (give color to #ff7200 hex codes) https://github.com/norcalli/nvim-colorizer.lua
+    'norcalli/nvim-colorizer.lua',
+    config = function() require('colorizer').setup() end,
+  },
 
-   { -- NOTE: DASHBOARD https://github.com/goolord/alpha-nvim/blob/main/lua/alpha/themes/dashboard.lua
-      'goolord/alpha-nvim',
-      dependencies = {
-         -- 'nvim-mini/mini.icons',
-         'nvim-tree/nvim-web-devicons',
-         'nvim-lua/plenary.nvim',
+  { -- NOTE: DASHBOARD https://github.com/goolord/alpha-nvim/blob/main/lua/alpha/themes/dashboard.lua
+    'goolord/alpha-nvim',
+    dependencies = {
+      -- 'nvim-mini/mini.icons',
+      'nvim-tree/nvim-web-devicons',
+      'nvim-lua/plenary.nvim',
+    },
+    -- config = function() require('alpha').setup(require('alpha.themes.theta').config) end,
+    config = function()
+      -- theta
+      -- local dash = require 'alpha.themes.theta' -- theta with devicons instead of mini
+      --
+      -- dash.file_icons.provider = 'devicons'
+      -- require('alpha').setup(theta.config)
+
+      -- dashboard-nvim
+      local dash = require 'alpha.themes.dashboard' -- theme dashboard
+      local find_conf = function() require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' } end
+      --
+      local butt = dash.button -- (sc, txt, keybind, keybind_opts)
+      dash.section.buttons.val = {
+        butt('n', '  [n]ew buffer', '<cmd>enew<CR>'),
+        butt('f', '  Find [f]ile', '<CMD>Telescope find_files<CR>'),
+        butt('r', '  [r]ecent files', '<CMD>Telescope oldfiles<CR>'),
+        butt('g', '󰈬  live [g]rep', '<CMD>Telescope live_grep<CR>'),
+        butt('h', '󰋖  find [h]elp', '<CMD>Telescope help_tags<CR>'),
+        butt('m', '  find [m]an_pages', '<CMD>Telescope man_pages<CR>'),
+        butt('c', ' find [c]onfig', find_conf), -- NOTE: ignore diagnose, it's wrong
+        -- butt('C', '  [C]olorschemes', '<CMD>Telescope colorscheme<CR>'),
+        butt('l', '󰒲  [l]azy', '<CMD>Lazy<CR>'),
+        -- butt('SPC f m', '  Jump to bookmarks'),
+        -- butt('SPC s l', '  Open last session'),
+        butt('q', '󰩈  [q]uit', '<cmd>q<CR>'),
+      }
+      dash.section.header.val = {
+        [[*------------------------------------------------*]],
+        [[|    .-----------------------.                   |]],
+        [[|    |  ▄▄         ▄       ▄▄|    .-----.        |]],
+        [[|    |▄▀███▄     ▄██     ▄███|    | === |        |]],
+        [[|    |██▄▀███▄   ███   ▄███▀ |    |-----|        |]],
+        [[|    |███  ▀███▄ ███ ▄███▀   |    | === |        |]],
+        [[|    |███    ▀██ ███ ███▄    |    |-----|        |]],
+        [[|    |███      ▀ ███  ▀███▄  |    |:::::|        |]],
+        [[|    |▀██        ▀██    ▀███▄|    | ::: |        |]],
+        [[|    |  ▀          ▀      ▀▀▀|    |____o|        |]],
+        [[|    ')---------------------('  ____________     |]],
+        [[|    '::::::::::'  '::::::::::' '  no mouse '    |]],
+        [[|   ':::========'  '==hjkl==:::' '  required '   |]],
+        [[|  '------------'  '------------' '-----------'  |]],
+        [[|              Powered by  eovim              |]],
+        [[*------------------------------------------------*]],
+      }
+      dash.section.footer.val = require 'alpha.fortune'() -- forture (https://github.com/goolord/alpha-nvim/blob/main/lua/alpha/fortune.lua)
+      require('alpha').setup(dash.opts)
+
+      -- -- startify
+      -- local dash = require 'alpha.themes.startify' -- startify
+      --
+      -- dash.file_icons.provider = 'devicons'
+      -- dash.section.mru.val = {} -- disable mru
+      -- -- dash.section.mru_cwd.val = {}
+      -- dash.section.footer.type = 'text' -- forture (https://github.com/goolord/alpha-nvim/blob/main/lua/alpha/fortune.lua)
+      -- dash.section.footer.val = require 'alpha.fortune'() -- forture (https://github.com/goolord/alpha-nvim/blob/main/lua/alpha/fortune.lua)
+      -- require('alpha').setup(dash.config)
+    end,
+  },
+
+  { -- NOTE: Lualine https://github.com/nvim-lualine/lualine.nvim
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      { 'tiagovla/scope.nvim', config = true }, -- NOTE: Scope -- https://github.com/tiagovla/scope.nvim
+    },
+
+    init = function()
+      vim.g.lualine_laststatus = vim.o.laststatus
+      if vim.fn.argc(-1) > 0 then
+        vim.o.statusline = ' ' -- set an empty statusline till lualine loads
+      else
+        vim.o.laststatus = 0 -- hide the statusline on the starter page
+      end
+    end,
+
+    config = function() -- opts = {},
+      require('lualine').setup {
+        -- opts = function()
+        --   local opts = {
+
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          -- component_separators = { left = '', right = '' },
+          -- section_separators = { left = '', right = '' },
+          component_separators = { left = '|', right = '|' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            'alpha',
+            'neo-tree',
+            -- statusline = { 'alpha', 'neo-tree' },
+            -- winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            -- Update if auto if no event
+            statusline = 1000,
+            -- tabline = 100,
+            -- winbar = 1000,
+            -- speed
+            refresh_time = 16, -- ~60fps
+            events = { -- events
+              'WinEnter',
+              'BufEnter',
+              'BufWritePost',
+              'SessionLoadPost',
+              'FileChangedShellPost',
+              'VimResized',
+              'Filetype',
+              'CursorMoved',
+              'CursorMovedI',
+              'ModeChanged',
+            },
+          },
+        },
+        sections = { -- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#component-options
+          lualine_a = {
+            'mode',
+          },
+          lualine_b = {
+            'branch',
+            'diff',
+            'diagnostics',
+          },
+          lualine_c = {
+            'filename',
+          }, -- 'filename' = '%t%m'
+          lualine_x = {
+            -- 'encoding',
+            -- 'fileformat',
+            'lsp_status',
+            'filetype',
+          },
+          lualine_y = {
+            'selectioncount',
+            'progress',
+            'location',
+          },
+          lualine_z = {
+            function() return ' ' .. os.date '%R' end,
+          },
+        },
+        -- inactive_sections = {
+        --   lualine_a = {},
+        --   lualine_b = {},
+        --   lualine_c = { 'filename' },
+        --   lualine_x = { 'location' },
+        --   lualine_y = {},
+        --   lualine_z = {},
+        -- },
+
+        -- -- tabline = {},
+        -- tabline = { -- TODO: replace with bufferline (or barbar)
+        --   lualine_a = { 'buffers' },
+        --   -- lualine_c = { '%=', '%t%m', '%3p' }, '%=' = ??? ; '%t%m' = filename,modified ; '%3p' = linenr/3
+        --   -- lualine_x = { 'g:coc_status', 'bo:filetype' }, -- ignores abcxyz position and do whatever
+        --   lualine_y = {},
+        --   lualine_z = { 'tabs' },
+        -- },
+
+        -- winbar = {},
+        -- inactive_winbar = {},
+        -- -- winbar = {
+        -- --    lualine_a = {},
+        -- --    lualine_b = {},
+        -- -- lualine_c = { 'filename' },
+        -- --    lualine_x = {},
+        -- --    lualine_y = {},
+        -- --    lualine_z = {},
+        -- -- },
+        -- -- inactive_winbar = {
+        -- --    lualine_a = {},
+        -- --    lualine_b = {},
+        -- -- lualine_c = { 'filename' },
+        -- --    lualine_x = {},
+        -- --    lualine_y = {},
+        -- --    lualine_z = {},
+        -- -- },
+
+        extensions = {
+          'neo-tree',
+          'lazy',
+          'man',
+          'mason',
+          'nvim-dap-ui',
+        },
+      }
+      local function mapo(key, cmd, description, mode) -- modified version of regular `map()`
+        mode = mode or 'n'
+        vim.keymap.set(mode, key, cmd, { noremap = true, silent = true, desc = description })
+      end
+      mapo('<leader>bb', '<cmd>e #<cr>', 'Switch to other')
+      mapo('<leader>bl', '<cmd>buffers<CR>', '[l]ist buffers')
+      mapo('<leader>bn', '<cmd>enew<CR>', 'new buf-file')
+      mapo('<leader>bd', '<cmd>bn<BAR>bd #<CR>', 'delete buf')
+      mapo('H', '<cmd>bp<CR>', 'prev buf')
+      mapo('L', '<cmd>bn<CR>', 'next buf')
+    end,
+  },
+
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    event = 'VeryLazy',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    keys = {
+      { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'toggle pin' },
+      { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'delete non-pinned buffers' },
+      { '<leader>bd', '<Cmd>bn <BAR> bd #<CR>', desc = 'delete buffer' },
+      { '<leader>bD', '<Cmd>BufferLinePickClose<CR>', desc = 'pick buf2DEL' },
+      -- TODO: for i in 1 10, "b<i>", "BufferLineGoToBuffer <i>", "goto buf <i>"
+      { '<leader>bs', '<cmd>BufferLinePick<cr>', desc = 'select buf' },
+      { '<S-M-h>', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
+      { '<S-M-l>', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+    },
+    opts = {
+      options = {
+        -- can be a string | function, | false see "Mouse actions"
+        close_command = 'bdelete! %d',
+        right_mouse_command = 'BufferLineTogglePin %d',
+        left_mouse_command = 'buffer %d',
+        middle_mouse_command = 'bdelete! %d',
+
+        indicator = {
+          icon = '👉', -- '▎', -- this should be omitted if indicator style is not 'icon'
+          style = 'icon', --'icon' | 'underline' | 'none',
+        },
+        modified_icon = ' ', -- '● ',
+        max_name_length = 15,
+        max_prefix_length = 12,
+        tab_tab_size = 20,
+
+        diagnostics = 'nvim_lsp',
+        always_show_bufferline = true,
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Neo-tree',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
       },
-      -- config = function() require('alpha').setup(require('alpha.themes.theta').config) end,
-      config = function()
-         -- theta
-         -- local dash = require 'alpha.themes.theta' -- theta with devicons instead of mini
-         --
-         -- dash.file_icons.provider = 'devicons'
-         -- require('alpha').setup(theta.config)
-
-         -- dashboard-nvim
-         local dash = require 'alpha.themes.dashboard' -- theme dashboard
-         local find_conf = function() require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' } end
-         --
-         local butt = dash.button -- (sc, txt, keybind, keybind_opts)
-         dash.section.buttons.val = {
-            butt('n', '  [n]ew buffer', '<cmd>enew<CR>'),
-            butt('f', '  Find [f]ile', '<CMD>Telescope find_files<CR>'),
-            butt('r', '  [r]ecent files', '<CMD>Telescope oldfiles<CR>'),
-            butt('g', '󰈬  live [g]rep', '<CMD>Telescope live_grep<CR>'),
-            butt('h', '󰋖  find [h]elp', '<CMD>Telescope help_tags<CR>'),
-            butt('m', '  find [m]an_pages', '<CMD>Telescope man_pages<CR>'),
-            butt('c', ' find [c]onfig', find_conf), -- NOTE: ignore diagnose, it's wrong
-            -- butt('C', '  [C]olorschemes', '<CMD>Telescope colorscheme<CR>'),
-            butt('l', '󰒲  [l]azy', '<CMD>Lazy<CR>'),
-            -- butt('SPC f m', '  Jump to bookmarks'),
-            -- butt('SPC s l', '  Open last session'),
-            butt('q', '󰩈  [q]uit', '<cmd>q<CR>'),
-         }
-         dash.section.header.val = {
-            [[*------------------------------------------------*]],
-            [[|    .-----------------------.                   |]],
-            [[|    |  ▄▄         ▄       ▄▄|    .-----.        |]],
-            [[|    |▄▀███▄     ▄██     ▄███|    | === |        |]],
-            [[|    |██▄▀███▄   ███   ▄███▀ |    |-----|        |]],
-            [[|    |███  ▀███▄ ███ ▄███▀   |    | === |        |]],
-            [[|    |███    ▀██ ███ ███▄    |    |-----|        |]],
-            [[|    |███      ▀ ███  ▀███▄  |    |:::::|        |]],
-            [[|    |▀██        ▀██    ▀███▄|    | ::: |        |]],
-            [[|    |  ▀          ▀      ▀▀▀|    |____o|        |]],
-            [[|    ')---------------------('  ____________     |]],
-            [[|    '::::::::::'  '::::::::::' '  no mouse '    |]],
-            [[|   ':::========'  '==hjkl==:::' '  required '   |]],
-            [[|  '------------'  '------------' '-----------'  |]],
-            [[|              Powered by  eovim              |]],
-            [[*------------------------------------------------*]],
-         }
-         dash.section.footer.val = require 'alpha.fortune'() -- forture (https://github.com/goolord/alpha-nvim/blob/main/lua/alpha/fortune.lua)
-         require('alpha').setup(dash.opts)
-
-         -- -- startify
-         -- local dash = require 'alpha.themes.startify' -- startify
-         --
-         -- dash.file_icons.provider = 'devicons'
-         -- dash.section.mru.val = {} -- disable mru
-         -- -- dash.section.mru_cwd.val = {}
-         -- dash.section.footer.type = 'text' -- forture (https://github.com/goolord/alpha-nvim/blob/main/lua/alpha/fortune.lua)
-         -- dash.section.footer.val = require 'alpha.fortune'() -- forture (https://github.com/goolord/alpha-nvim/blob/main/lua/alpha/fortune.lua)
-         -- require('alpha').setup(dash.config)
-      end,
-   },
-
-   { -- NOTE: Lualine https://github.com/nvim-lualine/lualine.nvim
-      'nvim-lualine/lualine.nvim',
-      dependencies = {
-         'nvim-tree/nvim-web-devicons',
-         { 'tiagovla/scope.nvim', config = true }, -- NOTE: Scope -- https://github.com/tiagovla/scope.nvim
-      },
-
-      config = function() -- opts = {},
-         require('lualine').setup {
-            options = {
-               icons_enabled = true,
-               theme = 'auto',
-               -- component_separators = { left = '', right = '' },
-               -- section_separators = { left = '', right = '' },
-               component_separators = { left = '|', right = '|' },
-               section_separators = { left = '', right = '' },
-               disabled_filetypes = {
-                  -- statusline = { 'lua' },
-                  statusline = {},
-                  winbar = {},
-               },
-               ignore_focus = {},
-               always_divide_middle = true,
-               always_show_tabline = true,
-               globalstatus = false,
-               refresh = {
-                  -- Update if auto if no event
-                  statusline = 1000,
-                  tabline = 100,
-                  winbar = 1000,
-                  -- speed
-                  refresh_time = 16, -- ~60fps
-                  events = { -- events
-                     'WinEnter',
-                     'BufEnter',
-                     'BufWritePost',
-                     'SessionLoadPost',
-                     'FileChangedShellPost',
-                     'VimResized',
-                     'Filetype',
-                     'CursorMoved',
-                     'CursorMovedI',
-                     'ModeChanged',
-                  },
-               },
-            },
-            sections = { -- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#component-options
-               lualine_a = { 'mode' },
-               lualine_b = { 'branch', 'diff', 'diagnostics' },
-               lualine_c = { 'filename' },
-               -- lualine_b = { 'branch', 'diff' },
-               -- lualine_c = { 'diagnostics' },
-               lualine_x = { 'encoding', 'fileformat', 'filetype' },
-               lualine_y = { 'progress' },
-               lualine_z = { 'location' },
-            },
-            inactive_sections = {
-               lualine_a = {},
-               lualine_b = {},
-               lualine_c = { 'filename' },
-               lualine_x = { 'location' },
-               lualine_y = {},
-               lualine_z = {},
-            },
-
-            -- tabline = {},
-            tabline = { -- TODO: replace with bufferline (or barbar)
-               lualine_a = { 'buffers' },
-               -- lualine_b = { 'branch' },
-               -- lualine_c = { 'filename' },
-               lualine_x = {},
-               lualine_y = {},
-               lualine_z = { 'tabs' },
-            },
-
-            winbar = {},
-            inactive_winbar = {},
-            -- winbar = {
-            --    lualine_a = {},
-            --    lualine_b = {},
-            -- lualine_c = { 'filename' },
-            --    lualine_x = {},
-            --    lualine_y = {},
-            --    lualine_z = {},
-            -- },
-            -- inactive_winbar = {
-            --    lualine_a = {},
-            --    lualine_b = {},
-            -- lualine_c = { 'filename' },
-            --    lualine_x = {},
-            --    lualine_y = {},
-            --    lualine_z = {},
-            -- },
-
-            extensions = {
-               'neo-tree',
-               'lazy',
-               'man',
-               'mason',
-               'nvim-dap-ui',
-            },
-         }
-
-         local map = vim.api.nvim_set_keymap
-         local opts = { noremap = true, silent = true }
-         -- Move to previous/next
-         map('n', '<leader>bh', '<Cmd>bprev<CR>', opts)
-         map('n', '<leader>bl', '<Cmd>bnext<CR>', opts)
-         -- Close buffer
-         map('n', '<leader>bd', '<Cmd>bnext | bdel #<CR>', opts)
-      end,
-   },
+    },
+  },
 }
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,54 +1,54 @@
 return { -- Useful plugin to show you pending keybinds.
-   'folke/which-key.nvim',
-   event = 'VimEnter',
-   ---@module 'which-key'
-   ---@type wk.Opts
-   ---@diagnostic disable-next-line: missing-fields
-   opts = {
-      preset = 'helix', -- false|"classic"|"modern"|"helix"
-      -- delay between pressing a key and opening which-key (milliseconds)
-      delay = 0,
-      icons = { mappings = vim.g.have_nerd_font },
+  'folke/which-key.nvim',
+  event = 'VimEnter',
+  ---@module 'which-key'
+  ---@type wk.Opts
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    preset = 'helix', -- false|"classic"|"modern"|"helix"
+    -- delay between pressing a key and opening which-key (milliseconds)
+    delay = 0,
+    icons = { mappings = vim.g.have_nerd_font },
 
-      -- Document existing key chains
-      spec = {
-         -- { '<leader>s', group = 'search', mode = { 'n', 'v' } },
-         -- { '<leader>t', group = 'toggle' },
-         { '<leader>gh', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
-         { 'gr', group = 'LSP Actions', mode = { 'n' } },
+    -- Document existing key chains
+    spec = {
+      -- { '<leader>s', group = 'search', mode = { 'n', 'v' } },
+      -- { '<leader>t', group = 'toggle' },
+      { '<leader>gh', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
+      { 'gr', group = 'LSP Actions', mode = { 'n' } },
 
-         -- HACK: add more groups below
+      -- HACK: add more groups below
 
-         -- <leader>
-         { '<leader>c', group = 'code', mode = { 'n' } },
-         { '<leader>d', group = 'debug', mode = { 'n' } },
-         { '<leader>g', group = 'git', mode = { 'n' } },
-         { '<leader>i', group = 'insert', mode = { 'n' } },
-         { '<leader>l', group = 'lazy', mode = { 'n' } },
-         { '<leader>m', group = 'text/[m]arkdown', mode = { 'n' } },
-         { '<leader>t', group = 'terminal' },
-         { '<leader>u', group = 'ui', mode = { 'n' } },
-         { '<leader><tab>', group = 'tab', mode = { 'n' } },
-         { '<leader>s', group = 'search/[s]elect', mode = { 'n', 'v' } },
-         { '<leader>f', group = 'find/[f]iles', mode = { 'n', 'v' } },
+      -- <leader>
+      { '<leader>c', group = 'code', mode = { 'n' } },
+      { '<leader>d', group = 'debug', mode = { 'n' } },
+      { '<leader>g', group = 'git', mode = { 'n' } },
+      { '<leader>i', group = 'insert', mode = { 'n' } },
+      { '<leader>l', group = 'lazy', mode = { 'n' } },
+      { '<leader>m', group = 'text/[m]arkdown', mode = { 'n' } },
+      { '<leader>t', group = 'terminal' },
+      { '<leader>u', group = 'ui', mode = { 'n' } },
+      { '<leader><tab>', group = 'tab', mode = { 'n' } },
+      { '<leader>s', group = 'search/[s]elect', mode = { 'n', 'v' } },
+      { '<leader>f', group = 'find/[f]iles', mode = { 'n', 'v' } },
 
-         -- <leader> subgroups
-         { '<leader>bo', group = 'order', mode = { 'n' } },
+      -- <leader> subgroups
+      { '<leader>bo', group = 'order', mode = { 'n' } },
 
-         -- goto
-         { 'gs', group = 'surround', mode = { 'n', 'v' } },
+      -- goto
+      { 'gs', group = 'surround', mode = { 'n', 'v' } },
 
-         {
-            '<leader>b',
-            group = '[b]buffer',
-            expand = function() return require('which-key.extras').expand.buf() end,
-         },
-         {
-            '<leader>w',
-            group = '[w]windows',
-            proxy = '<c-w>',
-            expand = function() return require('which-key.extras').expand.win() end,
-         },
+      {
+        '<leader>b',
+        group = '[b]buffer',
+        expand = function() return require('which-key.extras').expand.buf() end,
       },
-   },
+      {
+        '<leader>w',
+        group = '[w]windows',
+        proxy = '<c-w>',
+        expand = function() return require('which-key.extras').expand.win() end,
+      },
+    },
+  },
 }
