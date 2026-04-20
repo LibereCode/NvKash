5f3d557b7e74cc8b4d56d35f1bc64e2ef44716f0
Libere Code <libere-code@posteo.org>
2026-04-20 14:21:33 +0200

    mv git.lua util/gitsigns.lua

--- lua/plugins/edit/init.lua
+++ lua/plugins/edit/init.lua
@@ -3,4 +3,7 @@ return {
   require 'plugins.edit.autopairs',
   require 'plugins.edit.undotree',
   require 'plugins.edit.grug',
+
+  -- Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
+  { 'NMAC427/guess-indent.nvim', opts = { enabled = false } },
 }
--- lua/plugins/util/init.lua
+++ lua/plugins/util/init.lua
@@ -1,5 +1,15 @@
 return {
-  -- TODO: add
-  -- git
-  -- oneliners?
+
+  require 'plugins.util.gitsigns',
+
+  -- sudo if write passwd in a non-root nvim -- https://github.com/lambdalisue/vim-suda
+  {
+    'lambdalisue/suda.vim',
+    lazy = false,
+    -- has to be config caus it is a .vim plugin
+    config = function() vim.api.nvim_create_user_command('W', 'echo "Just type :SudaWrite"', {}) end,
+  },
+
+  -- nvim v0.8.0
+  -- { 'kdheepak/lazygit.nvim' } -- HACK: replaced with custom.lazygit
 }
