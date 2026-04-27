aae6321ffefe22147edd6b73caa8142ed19d10e6
Libere Code <libere-code@posteo.org>
2026-04-27 16:59:12 +0200

    vim.vim + lua.lua

--- after/ftplugin/help.vim
+++ after/ftplugin/help.vim
@@ -3,9 +3,29 @@
 " NOTE: lua HEREDOC
 lua << EOF
 -- INFO: Simply a copy of the same mapping in `./lua.lua`, but put here so that if you open help from a non-lua file, you still get access
-local map = vim.keymap.set
-map('n', '<localleader>h', function()
+
+---@param key string
+---@param cmd string|function
+---@param optsExtra table? -- **OPTIONAL** locked having `buf = 0`
+---@param mode (string|table<string>)? -- **OPTIONAL** defaults to 'n'
+local function map(key, cmd, optsExtra, mode)
+  mode = mode or 'n'
+  local opts = vim.tbl_extend('error', { buf = 0 }, optsExtra)
+  vim.keymap.set(mode, key, cmd, opts)
+end
+
+map('<localleader>h', function()
   local hword = vim.fn.expand '<cword>'
   vim.cmd('h ' .. hword)
 end, { desc = 'help <cword>', remap = true })
+
+local function helpCword() -- Soo dumb (returning a function like that)... but it works?
+  return function()
+    local hword = vim.fn.expand '<cword>'
+    vim.cmd('h ' .. hword)
+  end
+end
+map('<localleader>h', helpCword(), { desc = 'help <cword>', remap = true })
+map('gh', helpCword(), { desc = 'help <cword>', remap = true }) -- will replace the ... weird? default gh ``:h gh`
+
 EOF
--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,13 +1,27 @@
-local map = vim.keymap.set
+---@param key string
+---@param cmd string|function
+---@param optsExtra table? -- **OPTIONAL** locked having `buf = 0`
+---@param mode (string|table<string>)? -- **OPTIONAL** defaults to 'n'
+local function map(key, cmd, optsExtra, mode)
+  mode = mode or 'n'
+  local opts = vim.tbl_extend('error', { buf = 0 }, optsExtra)
+  vim.keymap.set(mode, key, cmd, opts)
+end
 
-map({ 'n', 'x' }, '<localleader>r', function() vim.cmd 'source' end, { desc = '[r]un Lua' })
+map('<localleader>r', function() vim.cmd 'source' end, { desc = '[r]un Lua' }, { 'n', 'x' })
 
-map('n', '<localleader>R', function() vim.cmd([[ terminal nvim -l ]] .. vim.fn.expand '%:p') end, { desc = '[R]un Lua (nvim -l)', silent = true })
+map('<localleader>R', function() vim.cmd([[ terminal nvim -l ]] .. vim.fn.expand '%:p') end, { desc = '[R]un Lua (nvim -l)', silent = true })
 
-map('n', '<localleader>h', function()
-  local hword = vim.fn.expand '<cword>'
-  vim.cmd('h ' .. hword)
-end, { desc = 'help <cword>', remap = true })
+---@return function -- Soo dumb (returning a function like that)... but it works?
+local function helpCword() -- return a function() that search help of the current word
+  return function()
+    local hword = vim.fn.expand '<cword>'
+    vim.cmd('h ' .. hword)
+  end
+end
+map('<localleader>h', helpCword(), { desc = 'help <cword>', remap = true })
+map('gh', helpCword(), { desc = 'help <cword>', remap = true }) -- will replace the ... weird?
+-- default gh ``:h gh`. Just use <C-g> while in V-mode instead....
 
 local o = vim.opt_local
 o.softtabstop = 2 -- . :h 'sts' -- Will only be applied at config because :...
